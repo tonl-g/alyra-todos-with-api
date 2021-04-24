1d11c492e440a90a2d5e43ee37fbc2a42899cb81
PeHaa <info@pehaa.com>
2021-04-24 19:38:51 +0200

    upate tasks

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -6,19 +6,29 @@ import { v4 as uuidv4 } from "uuid"
 
 const initialTodos = [
   {
-    text: "Faires des courses",
+    text: "Forkez et cloner ce repo",
     isCompleted: true,
     id: "1b688c51-e990-4ce3-95a5-9018cf81d23d"
   },
   {
-    text: "Réviser ES6 classes",
+    text: "Jouer avec le <title></title> 🤩",
     isCompleted: false,
     id: "efc6331d-7ca2-49a6-b014-378b8280b33d"
   },
   {
-    text: "Aroser les plantes",
+    text: "Enregistrer les tâches dans localStorage 🤓",
     isCompleted: false,
     id: "9e60d353-cd72-40bb-97e6-5841e51635c0"
+  },
+  {
+    text: "Mettre en place dark mode 😎",
+    isCompleted: false,
+    id: "df0ce18c-b4fa-4651-82c0-72fad6b486e4"
+  },
+  {
+    text: "Enregistret dark mode dans localStorage 🥳",
+    isCompleted: false,
+    id: "206e8742-02b1-4ce9-92d2-d6184588f4c3"
   }
 ]
 
